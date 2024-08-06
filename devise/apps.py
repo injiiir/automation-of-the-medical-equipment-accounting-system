ede1518f68ce9e59c3f501085c0046f00db13b5c
injiiir <lnika2509@gmail.com>
2024-08-06 17:10:16 +0300

    Update apps.py

--- devise/apps.py
+++ devise/apps.py
@@ -1,2 +1,12 @@
-git rm apps.py
+from django.apps import AppConfig
+
+
+class ApiConfig(AppConfig):
+    # default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api'
+
+
+class DeviceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'device'
 
